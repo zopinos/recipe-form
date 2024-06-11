13cec823857f1d1809a209a06830aff9ba311ce2
zopinos <zopi.tv@gmail.com>
2024-06-11 22:38:26 +0300

    unit conversion work

--- recipe-react-app/src/tests/unitConversion.test.js
+++ recipe-react-app/src/tests/unitConversion.test.js
@@ -1,19 +1,28 @@
 /* eslint-disable no-undef */
 import { expect, test } from 'vitest'
-import { hasUnit, findUnit } from '../util/unitConversion.js'
+import {
+    hasUnit,
+    findUnit,
+    hasQuantity,
+    findQuantity,
+    hasConvertableAmount,
+    convertAmount
+} from '../util/unitConversion.js'
+import { allUnits } from '../util/constants.js'
 
 test('tests work', () => {
     console.log("hello  world!".split(/\s+/))
+    console.log(allUnits)
 
     expect(true).toBe(true)
 })
 
 describe('hasUnit', () => {
-    test('returns true when unit in a string', () => {
+    test('returns true when a unit is found in a string', () => {
         expect(hasUnit('5 dl')).toStrictEqual(true)
-      })
+    })
 
-    test('returns false when unit is not found in a string', () => {
+    test('returns false when a unit is not found in a string', () => {
         expect(hasUnit('5')).toStrictEqual(false)
     })
 
@@ -25,9 +34,60 @@ describe('hasUnit', () => {
 describe('findUnit', () => {
     test('returns the found unit when unit in a string', () => {
         expect(findUnit('5 dl')).toStrictEqual('dl')
-      })
+    })
 
     test('returns falsy when unit is not found in a string', () => {
         expect(findUnit('5')).toBeFalsy()
+    })
+})
+
+describe('hasQuantity', () => {
+    test('returns true when quantity is in string', () => {
+        expect(hasQuantity('5 dl', 'dl')).toStrictEqual(true)
+    })
+
+    test('returns false when quantity is not in string', () => {
+        expect(hasQuantity('viisi dl', 'dl')).toStrictEqual(false)
+    })
+})
+
+describe('findQuantity', () => {
+    test('returns the found quantity as a Number when amount is correctly formatted in a string', () => {
+        expect(findQuantity('5 dl', 'dl')).toStrictEqual(5)
+    })
+
+    test('returns falsey when quantity is not in string', () => {
+        expect(hasQuantity('viisi dl', 'dl')).toBeFalsy()
+    })
+})
+
+describe('hasConvertableAmount', () => {
+    test('returns true if a convertable amount can be found in a string', () => {
+        expect(hasConvertableAmount('5 dl')).toStrictEqual(true)
+    })
+
+    test('returns false if a convertable amount cannot be found in a string', () => {
+        expect(hasConvertableAmount('viisi dl')).toStrictEqual(false)
+        expect(hasConvertableAmount('5 not-a-unit')).toStrictEqual(false)
+    })
+})
+
+describe('convertAmount', () => {
+    test('returns correctly converted amount (volume)', () => {
+        expect(convertAmount('5 l', 'dl')).toStrictEqual('50 dl')
+        expect(convertAmount('50 dl', 'l')).toStrictEqual('5 l')
+    })
+
+    test('returns correctly converted amount (little more complex volume)', () => {
+        expect(convertAmount('1 tl', 'dl')).toStrictEqual('0.05 dl')
+    })
+
+    test('returns correctly converted amount (complex volume)', () => {
+        expect(convertAmount('1 dl', 'rkl')).toStrictEqual('6.6666 rkl')
+    })
+
+    test('returns correctly converted amount (mass)', () => {
+        expect(convertAmount('5 kg', 'g')).toStrictEqual('5000 g')
+        expect(convertAmount('5000 g', 'kg')).toStrictEqual('5 kg')
     })
 })
--- recipe-react-app/src/util/unitConversion.js
+++ recipe-react-app/src/util/unitConversion.js
@@ -1,7 +1,7 @@
-import unitConversions from './unitConversions.json' assert { type: 'json'}
+import { allUnits, volumes, masses, conversions } from './constants'
 
 export const findUnit = (amount) => {
-    return unitConversions.allUnits
+    return allUnits
         .find(unit => 
             amount.split(/\s+/).includes(unit)
         )
@@ -11,8 +11,39 @@ export const hasUnit = (amount) => {
     return findUnit(amount) ? true : false
 }
 
-const unitConverter = (amount) => {
-    return amount
+export const findQuantity = (amount, foundUnit) => {
+    const parts = amount.split(/\s+/)
+    const idx = parts.indexOf(foundUnit)
+
+    if (!(idx > 0)) return;
+
+    return Number(parts[idx-1])
 }
 
-export default unitConverter
+export const hasQuantity = (amount, foundUnit) => {
+    return findQuantity(amount, foundUnit) ? true : false
+}
+
+export const hasConvertableAmount = (amount) => {
+    const foundUnit = findUnit(amount)
+    return (!!foundUnit) && hasQuantity(amount, foundUnit)
+}
+
+export const convertAmount = (amount, target) => {
+    const foundUnit = findUnit(amount)
+    const foundQuantity = findQuantity(amount, foundUnit)
+
+    if (volumes.includes(foundUnit)) {
+        const ratioToBase = 1 / conversions.volume[foundUnit]
+        const asBaseUnit = foundQuantity * ratioToBase
+        const ratioToTarget = conversions.volume[target]
+
+        return `${asBaseUnit * ratioToTarget} ${target}`
+    } else if (masses.includes(foundUnit)) {
+        const ratioToBase = 1 / conversions.mass[foundUnit]
+        const asBaseUnit = foundQuantity * ratioToBase
+        const ratioToTarget = conversions.mass[target]
+
+        return `${asBaseUnit * ratioToTarget} ${target}`
+    }
+}
