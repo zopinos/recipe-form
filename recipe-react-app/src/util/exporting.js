096db133a58ab629489de94807aba5e37a30530e
zopinos <zopi.tv@gmail.com>
2024-06-24 17:16:33 +0300

    updated json exporting

--- recipe-react-app/src/util/exporting.js
+++ recipe-react-app/src/util/exporting.js
@@ -1,19 +1,10 @@
 const exportObjectAsJSON = (filename, dataObjToWrite) => {
-  const blob = new Blob([JSON.stringify(dataObjToWrite, null, '\t')], { type: 'application/json' });
+  const blob = new Blob([JSON.stringify(dataObjToWrite, null, 2)], { type: 'application/json' });
   const link = document.createElement('a');
-
+  link.href = URL.createObjectURL(blob);
   link.download = filename;
-  link.href = window.URL.createObjectURL(blob);
-  link.dataset.downloadurl = ['application/json', link.download, link.href].join(':');
-
-  const evt = new MouseEvent('click', {
-    view: window,
-    bubbles: true,
-    cancelable: true,
-  });
-
-  link.dispatchEvent(evt);
-  link.remove();
+  link.click();
+  URL.revokeObjectURL(link.href);
 };
 
 export default exportObjectAsJSON;
