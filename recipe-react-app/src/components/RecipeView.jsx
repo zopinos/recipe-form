89d5a01cd504f010d7a4a896cb17687a6a3daf1b
zopinos <zopi.tv@gmail.com>
2024-07-28 20:25:53 +0300

    recipe view work

--- recipe-react-app/src/components/RecipeView.jsx
+++ recipe-react-app/src/components/RecipeView.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 
 const RecipeView = () => {
@@ -9,19 +10,35 @@ const RecipeView = () => {
   return (
     <div className='recipe-view'>
       <h1>{title}</h1>
-      {portions.included && <span>{portions.portions} annosta</span>}
+      {portions.included && <div className='portions'>{portions.portions} annosta</div>}
       {ingredients.map(ingredientList => 
         <div key={ingredientList.id}>
-          <h2>{ingredientList.title}</h2>
-          {ingredientList.ingredients.map(ingredient =>
-            <span key={ingredient.id}>{ingredient.amount} {ingredient.name}</span>
-          )}
+          <table>
+            <caption>
+              <h2>{ingredientList.title}</h2>
+            </caption>
+            <tbody>
+              {ingredientList.ingredients.map(ingredient =>
+                <tr key={ingredient.id}>
+                  <td className='table-ingredient-amount'>{ingredient.amount}</td>
+                  <td className='table-ingredient-name'>{ingredient.name}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
       )}
       {textContents.map(textContent =>
         <div key={textContent.id}>
           <h2>{textContent.title}</h2>
-          <p>{textContent.text}</p>
+          <p>
+            {textContent.text && textContent.text.split('\n').map((text, index) => (
+              <Fragment key={index}>
+                {text}
+                <br />
+              </Fragment>
+            ))}
+          </p>
         </div>
       )}
     </div>
