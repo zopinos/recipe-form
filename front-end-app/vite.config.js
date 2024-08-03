a744b4fe7fc5ef651a189674cddfb3fb85cb2104
zopinos <zopi.tv@gmail.com>
2024-08-03 15:19:22 +0300

    New build

--- front-end-app/vite.config.js
+++ front-end-app/vite.config.js
@@ -1,3 +1,18 @@
+/* // Web
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './testSetup.js', 
+  }
+}); */
+
+// Single File
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteSingleFile } from 'vite-plugin-singlefile';
